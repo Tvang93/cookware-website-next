7610bb1d2153755be9f803a7620e8f2b7a9d2e26
Tvang93 <vangt39@gmail.com>
2025-06-11 13:51:42 -0700

    new arrivals, working on best sellers and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ const page = () => {
   const startCookingCards = (num: number, text: string) => {
     return (
       <div className="flex flex-col">
-        <div className="overflow-hidden rounded-lg h-87 w-68 hover:cursor-pointer">
+        <div className="overflow-hidden rounded-lg max-h-100.5 max-w-95 hover:cursor-pointer">
           <img
-            className="h-full hover:scale-125"
+            className="w-85 h- hover:scale-125"
             src={`/assets/StartCooking${num}.jpeg`}
             alt={text}
           />
@@ -37,41 +37,90 @@ const page = () => {
           Sale $10 off when you spend $150
         </p>
       </div>
-      {/* Start Cooking */}
-      <div className="flex flex-col items-center bg-[#ededed]">
-        <h1 className="text-6xl mt-28 mb-13 font-bold">Start Cooking</h1>
-        <div className="flex gap-12">
-          {startCookingCards(1, "Cast Iron Pot")}
-          {startCookingCards(2, "Stock Pot")}
-          {startCookingCards(3, "Sauce Pan")}
-          {startCookingCards(4, "Accessories")}
+      {/* body */}
+      <div className="bg-[#ededed] ps-29 pe-40">
+        {/* Start Cooking */}
+        <div className="flex flex-col items-center ">
+          <h1 className="text-6xl mt-28 mb-13 font-bold">Start Cooking</h1>
+          <div className="grid grid-cols-4 gap-12">
+            {startCookingCards(1, "Cast Iron Pot")}
+            {startCookingCards(2, "Stock Pot")}
+            {startCookingCards(3, "Sauce Pan")}
+            {startCookingCards(4, "Accessories")}
+          </div>
         </div>
-      </div>
-      {/* New Arrivals */}
-      <div className="flex flex-col items-center bg-[#ededed]">
-        <h1 className="text-6xl mt-28 mb-13 font-bold">New Arrivals</h1>
-        <div className="grid grid-cols-2 gap-10">
-          <div className="flex flex-col items-start">
-            <div className="">
-              <img src="/assets/NewArrivals1.jpg" alt="placing crock pot in oven" />
+        {/* New Arrivals */}
+        <div className="flex flex-col items-center">
+          <h1 className="text-6xl mt-28 mb-13 font-bold">New Arrivals</h1>
+          <div className="grid grid-cols-2 gap-10">
+            <div className="flex flex-col items-start">
+              <div className="hover:brightness-75 overflow-hidden lg:h-118 2xl:h-158  @min-[1600px]:w-201">
+                <img
+                  className="h-full w-full"
+                  src="/assets/NewArrivals1.jpg"
+                  alt="placing crock pot in oven"
+                />
+              </div>
+              <h1 className="text-4xl font-bold mt-6">Fall Collection</h1>
+              <p className="mt-4">
+                Our 2023 Fall Collection just came out with beautiful autumn
+                color cookware.
+              </p>
+              <div>
+                <ColorSelectionComponent />
+              </div>
             </div>
-            <h1 className="text-4xl">Fall Collection</h1>
-            <p>
-              Our 2023 Fall Collection just came out with beautiful autumn color
-              cookware.
-            </p>
-            <div>
-              <ColorSelectionComponent />
+            <div className="flex flex-col items-start">
+              <div className="hover:brightness-75 overflow-hidden lg:h-118 2xl:h-158  @min-[1600px]:w-201">
+                <img src="/assets/NewArrivals2.jpg" alt="wok with food" />
+              </div>
+              <h1 className="text-4xl font-bold mt-6">Nonstick Wok 14 inch</h1>
+              <p className="mt-4">
+                The nonstick wok is one of our most popular cookware. Perfect
+                for everyday stir fry.
+              </p>
             </div>
           </div>
-          <div className="flex flex-col items-start">
+        </div>
+        {/* Best Sellers */}
+        <div className="flex flex-col items-center">
+          <h1 className="text-6xl mt-28 mb-13 font-bold">Best Sellers</h1>
+          <div>
+            <div>cards</div>
+          </div>
+        </div>
+      </div>
+      {/* Footer */}
+      <div className="bg-[#1e2c20] ps-29 pe-40">
+        <div className="flex justify-between">
+          <div className="grid lg:grid-cols-3 gap-15">
+            <ul className="text-white flex flex-col items-start">
+              <h1 className="font-bold text-xl">ABOUT OUR HOME</h1>
+              <li className="font-extralight">ABOUT US</li>
+              <li className="font-extralight">CAREER</li>
+              <li className="font-extralight">PRIVACY</li>
+              <li className="font-extralight">MEDIA RELATION</li>
+            </ul>
+            <ul className="text-white flex flex-col items-start">
+              <h1 className="font-bold text-xl">PRODUCTS</h1>
+              <li className="font-extralight">COOKWARE</li>
+              <li className="font-extralight">BAKEWARE</li>
+              <li className="font-extralight">BEST SELLERS</li>
+              <li className="font-extralight">ACCESSORIES</li>
+            </ul>
+            <ul className="text-white flex flex-col items-start">
+              <h1 className="font-bold text-xl">CUSTOMER SERVICE</h1>
+              <li className="font-extralight">CONTACT US</li>
+              <li className="font-extralight">ORDER STATUS</li>
+              <li className="font-extralight">RETURNS & EXCHANGE</li>
+              <li className="font-extralight">FAQS</li>
+            </ul>
+          </div>
+          <div>
+            <p>Subscribe to our newsletter to be the first to know about new products and special events.</p>
             <div>
-              <img src="/assets/NewArrivals2.jpg" alt="wok with food" />
+              <input type="email" placeholder="Enter" />
             </div>
-            <h1 className="text-4xl">Nonstick Wok 14 inch</h1>
-            <p>
-              The nonstick wok is one of our most popular cookware. Perfect for everyday stir fry.
-            </p>
           </div>
         </div>
       </div>
--- src/components/ColorSelectionComponent.tsx
+++ src/components/ColorSelectionComponent.tsx
@@ -6,26 +6,26 @@ const ColorSelectionComponent = () => {
     const [color, setColor] = useState<string>("Apple Cinnamon")
 
   return (
-    <div className="flex flex-col items-start">
+    <div className="flex flex-col items-start mt-5 gap-4">
       <div className="flex">
-        <h2>Color: </h2>
-        <p className="font-bold">{color}</p>
+        <h2>Color:</h2>
+        <p className="font-bold ms-0.5">{color}</p>
       </div>
-      <div className="flex">
+      <div className="flex gap-2">
         <div
-          className={`bg-[#ea2d2c] rounded-full h-5.5 w-5.5 ${color == "Apple Cinnamon" ? `border-1` : ``}`}
+          className={`bg-[#ea2d2c] rounded-full h-5.5 w-5.5 ${color == "Apple Cinnamon" ? `border-2` : ``}`}
           onClick={() => setColor("Apple Cinnamon")}
         />
         <div 
-        className={`bg-[#fb5108] rounded-full h-5.5 w-5.5 ${color == "Orange Mango" ? `border-1` : ``}`}
+        className={`bg-[#fb5108] rounded-full h-5.5 w-5.5 ${color == "Orange Mango" ? `border-2` : ``}`}
         onClick={() => setColor("Orange Mango")}
         />
         <div 
-        className={`bg-[#d29d12] rounded-full h-5.5 w-5.5 ${color == "Lemon Meringue" ? `border-1` : ``}`}
+        className={`bg-[#d29d12] rounded-full h-5.5 w-5.5 ${color == "Lemon Meringue" ? `border-2` : ``}`}
         onClick={() => setColor("Lemon Meringue")}
         />
         <div 
-        className={`bg-[#353535] rounded-full h-5.5 w-5.5 ${color == "Black" ? `border-1` : ``}`}
+        className={`bg-[#353535] rounded-full h-5.5 w-5.5 ${color == "Black" ? `border-2` : ``}`}
         onClick={() => setColor("Black")}
         />
       </div>
